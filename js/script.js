bd685767f360526687040fcd17db6a39c05a6a2a
Ahmad <aalkhald@ford.com>
2019-11-02 14:24:13 -0400

    add

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let name = "test";
+console.log(` This is a ${name}`);
+
+let campclass = {
+    stu: 3,
+    tvs: 1,
+    isWarm: true,
+    ta: "Jonathan",
+    teacher: "Aisha"
+}
+/*
+class is the object name
+stu is the property 
+*/
+
+console.log(campclass)
+console.log(campclass.ta)
